686f65653f2ca155cb9f4b25dbb4b6b82027ed02
TeoTayenne <etu52276@henallux.be>
2024-05-19 14:30:46 +0200

    Country DATA

--- src/main/viewPackage/AddUserPanel.java
+++ src/main/viewPackage/AddUserPanel.java
@@ -1,10 +1,44 @@
 package main.viewPackage;
 
 import javax.swing.*;
+import java.util.Locale;
 
 public class AddUserPanel extends JPanel {
+
+    private JTextField username, password, email, dateOfBirth,city,phoneNumber,zipdCode,street,houseNumber;
+    private JComboBox<String> country,gender;
+
+
+
     public AddUserPanel() {
         JLabel label = new JLabel("Add User Panel");
         add(label);
+
+        username = new JTextField(20);
+        add(new JLabel("Username"));
+        add(username);
+        password = new JTextField(20);
+        add(new JLabel("Password"));
+        add(password);
+        email = new JTextField(20);
+        add(new JLabel("Email"));
+        add(email);
+        dateOfBirth = new JTextField(20);
+        city = new JTextField(20);
+        phoneNumber = new JTextField(20);
+        zipdCode = new JTextField(20);
+        street = new JTextField(20);
+        houseNumber = new JTextField(20);
+
+        String[] countries;
+        country = new JComboBox();
+        String [] genders = {"Man","Women","Other"};
+        gender = new JComboBox(genders);
+        add(new JLabel("Select Country"));
+        add(country);
+        add(new JLabel("Gender"));
+        add(gender);
+        setVisible(true);
+
     }
 }
--- src/main/viewPackage/Main.java
+++ src/main/viewPackage/Main.java
@@ -1,7 +1,55 @@
 package main.viewPackage;
 
+import main.dataAccessPackage.ConnectionDataAccess;
+
+import java.sql.Statement;
+import java.util.Locale;
+
+import main.exceptionPackage.ConnectionDataAccessException;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        MainWindow mainWindow = new MainWindow();
+        try {
+            addCountry();
+        } catch (ConnectionDataAccessException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+
+public  static void addCountry() throws ConnectionDataAccessException {
+        ConnectionDataAccess connectionDataAccess = new ConnectionDataAccess();
+        Connection connection = connectionDataAccess.getInstance();
+        try {
+            Statement statement = connection.createStatement();
+            String[] countries = getAllCountries();
+            for (String country : countries) {
+                String query = "INSERT INTO country (name) VALUES ('" + country + "')";
+                statement.executeUpdate(query);
+            }
+            System.out.printf("Countries added to the database.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private static String[] getAllCountries() throws ConnectionDataAccessException {
+        ConnectionDataAccess connectionDataAccess = new ConnectionDataAccess();
+        // Utiliser Locale pour obtenir une liste de tous les pays
+        String[] countryCodes = Locale.getISOCountries();
+        String[] countryNames = new String[countryCodes.length];
+
+        for (int i = 0; i < countryCodes.length; i++) {
+            Locale locale = new Locale("", countryCodes[i]);
+            countryNames[i] = locale.getDisplayCountry();
+        }
+        return countryNames;
     }
 }
